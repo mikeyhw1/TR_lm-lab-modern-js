e76b5e7f85bf9691b51b3c2b10330e92520a11e4
mikeyhw1 <70017382+mikeyhw1@users.noreply.github.com>
2023-01-30 19:33:26 +0000

    test: init

--- modern_javascript_exercises/functions/functions_1.js
+++ modern_javascript_exercises/functions/functions_1.js
@@ -17,6 +17,7 @@ people.forEach(logPerson);
 // Store the logPerson() function in a variable called performLogPerson
 
 // Your code here
+// test
 
 // Arrow Functions in a Variable
 // Write a version of logPerson() as an Arrow Function and store it in a variable called arrowVersionOfLogPerson
